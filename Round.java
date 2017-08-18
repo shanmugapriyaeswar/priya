0f73707246dbe1e2689b9778e7d9a91198c3e0f5
shanmugapriyaeswar <shanmugapriyaeswar1997@gmail.com>
2017-08-18 15:38:47 +0530

    Add files via upload

---
+++ Round.java
@@ -0,0 +1,14 @@
+package priya;
+import java.util.*;
+public class Round {
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Round number:"+Math.round(23.1));
+	
+		System.out.println("Round number:"+Math.round(23.8));
+
+	}
+
+}
