0123537ab4585b0534e32d3ed261a7a6aa5b5851
shanmugapriyaeswar <shanmugapriyaeswar1997@gmail.com>
2017-08-18 15:31:35 +0530

    Add files via upload

---
+++ Random.java
@@ -0,0 +1,15 @@
+package priya;
+import java.util.*;
+public class Random 
+
+{
+	public static void main(String srgs[])
+	{
+		System.out.println("Random number:" +Math.random());
+		System.out.println("Random number:" +Math.random());
+		System.out.println("Random number:" +Math.random());
+		
+	}
+
+}
+ 
