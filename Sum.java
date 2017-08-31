e635910741b3826769bf73cda7718f444f904fa3
shanmugapriyaeswar <shanmugapriyaeswar1997@gmail.com>
2017-08-31 14:31:57 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,21 @@
+package reference;
+import java.util.*;
+public class Sum {
+
+	public static void main(String[] args) 
+	{
+		
+		Scanner s=new Scanner(System.in);
+		int a=s.nextInt();
+		int b=s.nextInt();
+		int c=0;
+		c=b;
+		for(int i=0;i<b;i++)
+		{
+			c++;
+		}
+		System.out.println(c);
+		s.close();
+	}
+
+}
