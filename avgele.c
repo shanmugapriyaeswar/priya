8be7ee9ce8e54df3cb0629d20628cf28afe66087
shanmugapriyaeswar <shanmugapriyaeswar1997@gmail.com>
2017-08-31 11:35:07 +0530

    Add files via upload

---
+++ avgele.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a[20],i,sum=0;
+float avg;
+printf("Enter the array elements");
+for(i=0;i<10;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+sum=sum+a[i];
+avg=sum/10;
+}
+printf("Average element is %d",avg);
+return 0;
+}
