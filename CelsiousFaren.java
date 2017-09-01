2add15c64b4ac418c8a5253305a0e6d283fad494
shanmugapriyaeswar <shanmugapriyaeswar1997@gmail.com>
2017-09-01 11:14:04 +0530

    Add files via upload

---
+++ CelsiousFaren.java
@@ -0,0 +1,16 @@
+package pppppppp;
+import java.util.*;
+public class CelsiousFaren {
+
+	public static void main(String[] args)
+	{
+		float f;
+		int a;
+		Scanner s=new Scanner(System.in);
+		a=s.nextInt();
+		f=(a*9/5)+32;
+		System.out.println(f);
+		s.close();
+	}
+
+}
